1b99a1ed38e4ca3ae676e0e10b78e52a7d992d19
thread <thread@threadbox.net>
2026-06-25 00:09:38 -0400

    SYNCTHING_SYNC_PLAN.md phase 3
    (cherry picked from commit 0a2741873df726c5c7f710633d7837e26ad1e8c7)

--- server/src/modules/sync/sync.module.ts
+++ server/src/modules/sync/sync.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { SyncthingClientService } from './syncthing-client.service';
+import { SyncReconcilerService } from './sync-reconciler.service';
 
 @Module({
-  providers: [SyncthingClientService],
-  exports: [SyncthingClientService],
+  providers: [SyncthingClientService, SyncReconcilerService],
+  exports: [SyncthingClientService, SyncReconcilerService],
 })
 export class SyncModule {}
