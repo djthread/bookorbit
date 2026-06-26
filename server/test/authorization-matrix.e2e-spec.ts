f84a5b0b014fd94b949f586777fac09b79c21707
thread <thread@threadbox.net>
2026-06-26 18:07:57 -0400

    protect syncthing with permission properly
    (cherry picked from commit fd4915053a447ab086a3fe4678aa426cc15a1f07)

--- server/test/authorization-matrix.e2e-spec.ts
+++ server/test/authorization-matrix.e2e-spec.ts
@@ -675,6 +675,11 @@ describe('Authorization matrix (e2e)', () => {
           path: '/storygraph/settings',
           token: 'allPerms',
         },
+        [Permission.Syncthing]: {
+          method: 'GET',
+          path: '/syncthing/targets',
+          token: 'allPerms',
+        },
         [Permission.DemoRestricted]: {
           method: 'GET',
           path: '/notifications',
